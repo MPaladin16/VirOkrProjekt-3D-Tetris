58261c20d7a391c2d5f6f723db9adc49b34de0f4
Filip Gustetić <filip.gustetic@fer.hr>
2022-12-15 15:02:06 +0100

    Tetris piece breaks when it hits the floor

---
+++ Assets/Scripts/TetrisPieceScript.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisPieceScript : MonoBehaviour
+{
+    //explosion details
+    public int cubesPerAxis = 3;
+    public float radius = 0.5f;
+    public float force = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        //sudar s podom -> eksplozija
+        if (collision.gameObject.layer == 3)
+        {
+            force *= collision.relativeVelocity.y;
+            Invoke("Explode", 0);
+        }
+    }
+
+    private void Explode()
+    {
+        foreach(Transform childCube in this.transform)
+        {
+            for(int x = 0; x < cubesPerAxis; x++) {
+                for (int y = 0; y < cubesPerAxis; y++) {
+                    for (int z = 0; z < cubesPerAxis; z++) {
+                        CreateMiniCube(childCube, new Vector3(x, y, z));
+                    }
+                }
+            }
+        }
+
+        Destroy(this.gameObject);
+    }
+
+    private void CreateMiniCube(Transform childCube, Vector3 pos)
+    {
+        GameObject miniCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        Renderer rd = miniCube.GetComponent<Renderer>();
+        rd.material = childCube.GetComponent<Renderer>().material;
+
+        //ako tetris dio padne na komadice
+        miniCube.layer = 3;
+
+        miniCube.transform.localScale = this.transform.localScale / cubesPerAxis;
+
+        Vector3 firstMiniCubePos = transform.position - transform.localScale / 2 + miniCube.transform.localScale / 2;
+        miniCube.transform.position = firstMiniCubePos + Vector3.Scale(pos, miniCube.transform.localScale);
+
+        Rigidbody rb = miniCube.AddComponent<Rigidbody>();
+        rb.AddExplosionForce(force, transform.position, radius);
+
+        Destroy(miniCube, 3.5f);
+    }
+
+
+}
