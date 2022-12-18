70afb99aa0d7da5f28d86f66cb00713ac88f93e6
Emilia Haramina <emilia999dragon@gmail.com>
2022-12-18 21:39:28 +0100

    Highlights for hovering tetris pieces
    Added green highlights for hovering tetris pieces over collider network
    BUT doesn't work very well, especially when placed diagonally OR sometimes the OnTriggerEnter() and OnTriggerExit() functions don't work correctly

--- Assets/Scripts/ColliderScript.cs
+++ Assets/Scripts/ColliderScript.cs
@@ -6,12 +6,34 @@ using UnityEngine;
 public class ColliderScript : MonoBehaviour
 {
     [SerializeField] bool full;
+    [SerializeField] public MeshRenderer outlineRenderer;
+    [SerializeField] int rowIndex;
+    [SerializeField] int i;
+    [SerializeField] int j;
 
     private GameObject _parentCube;
     // Start is called before the first frame update
     void Start()
     {
+        if (rowIndex < 0 || i < 0 || j < 0) {
+            Debug.Log(this + " not properly set!");
+        }
+    }
 
+    public MeshRenderer GetOutlineRenderer() {
+        return outlineRenderer;
+    }
+
+    public int GetRowIndex() {
+        return rowIndex;
+    }
+
+    public int GetI() {
+        return i;
+    }
+
+    public int GetJ() {
+        return j;
     }
 
     public void SetFull(GameObject go)
--- Assets/Scripts/CubePlaceSetter.cs
+++ Assets/Scripts/CubePlaceSetter.cs
@@ -4,11 +4,169 @@ using UnityEngine;
 
 public class CubePlaceSetter : MonoBehaviour
 {
+    // not sure if it works right
+    List<ColliderScript> activeColliders = new List<ColliderScript>();
+
+    [SerializeField] public bool[] shapeRow1 = new bool[2];
+    [SerializeField] public bool[] shapeRow2 = new bool[2];
+    [SerializeField] public bool[] shapeRow3 = new bool[2];
+    [SerializeField] public bool[] shapeRow4 = new bool[2];
+
+    private int boolCount;
+    private List<ColliderScript> lastShape = new List<ColliderScript>();
+
+    void Start() {
+        int boolCount = 0;
+        foreach (bool b in shapeRow1) {
+            if (b) {
+                boolCount++;
+            }
+        }
+        foreach (bool b in shapeRow2) {
+            if (b) {
+                boolCount++;
+            }
+        }
+        foreach (bool b in shapeRow3) {
+            if (b) {
+                boolCount++;
+            }
+        }
+        foreach (bool b in shapeRow4) {
+            if (b) {
+                boolCount++;
+            }
+        }
+    }
+
+    void Update() {
+        foreach (ColliderScript cube in lastShape) {
+            cube.GetOutlineRenderer().enabled = false;
+        }
+
+        lastShape = correctShape();
+        if (lastShape.Count == 4) {
+            // Debug.Log("Shape: ");
+            // foreach (ColliderScript s in shape) {
+            //     Debug.Log(s);
+            // }
+
+            foreach (ColliderScript cube in lastShape) {
+                cube.GetOutlineRenderer().enabled = true;
+            }
+        }
+    }
+
+    private List<ColliderScript> correctShape() {
+        List<ColliderScript> shape = new List<ColliderScript>();
+
+        // List<string> shapeBool = new List<string>();
+        // // 2 = number of columns for shape
+        // for (int i = 0; i < 2; i++) {
+        //     if (shapeRow1[i]) {
+        //         shapeBool.Add("0" + i);
+        //     }
+        //     if (shapeRow2[i]) {
+        //         shapeBool.Add("1" + i);
+        //     }
+        //     if (shapeRow3[i]) {
+        //         shapeBool.Add("2" + i);
+        //     }
+        //     if (shapeRow4[i]) {
+        //         shapeBool.Add("3" + i);
+        //     }
+        // }
+
+        // // TODO: Create list with 6 functions for each direction
+
+        // List<ColliderScript> activeCollidersCopy = new List<ColliderScript>(activeColliders);
+        // foreach (ColliderScript middleCollider in activeCollidersCopy) {
+        //     shape = new List<ColliderScript>();
+        //     List<string> shapeBoolCopy = new List<string>(shapeBool);
+
+        //     foreach (ColliderScript otherCollider in activeCollidersCopy) {
+        //         if (middleCollider == otherCollider) {
+        //             continue;
+        //         }
+
+        //         // TODO: check all bools to see if they are in the correct position
+        //         if (middleCollider.GetI)
+        //     }
+        // }
+
+        if (activeColliders.Count == 4) {
+            Debug.Log("Active colliders: ");
+            foreach (ColliderScript s in activeColliders) {
+                Debug.Log(s);
+            }
+
+            List<ColliderScript> activeCollidersCopy = new List<ColliderScript>(activeColliders);
+            bool touching = false;
+
+            foreach (ColliderScript collider1 in activeCollidersCopy) {
+                touching = false;
+                foreach (ColliderScript collider2 in activeCollidersCopy) {
+                    if (collider1 == collider2) {
+                        continue;
+                    }
+
+                    int differenceRow = Mathf.Abs(collider1.GetRowIndex() - collider2.GetRowIndex());
+                    int differenceI = Mathf.Abs(collider1.GetI() - collider2.GetI());
+                    int differenceJ = Mathf.Abs(collider1.GetJ() - collider2.GetJ());
+                    if ((differenceI == 1 || differenceJ == 1 || differenceRow == 1) &&
+                        differenceI <= 1 && differenceJ <= 1 && differenceRow <= 1 &&
+                        !(differenceI == 1 && differenceJ == 1) &&
+                        !(differenceI == 1 && differenceRow == 1) &&
+                        !(differenceJ == 1 && differenceRow == 1)) {
+                            Debug.Log("Touching: " + collider1 + " and " + collider2);
+                            Debug.Log("Difference row: " + differenceRow + ", Difference I: " + differenceI + ", DifferenceJ: " + differenceJ);
+                            touching = true;
+                            // TODO: Could add collider2 as well
+                            shape.Add(collider1);
+                            break;
+                    }
+                }
+                if (!touching) {
+                    break;
+                }
+            }
+
+            if (!touching) {
+                return new List<ColliderScript>();
+            }
+        }
+
+        return shape;
+    }
+
     // Start is called before the first frame update
     public void setAllChildrenCubePosition() {
         foreach (var c in GetComponentsInChildren<CubeScript>())
         {
             c.SetCubePositions();
+        }
+    }
+
+    public void PutChildrenInNetwork() {
+
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "BoxHitCollider") {
+            ColliderScript collider = other.gameObject.GetComponent<ColliderScript>();
+            if (!activeColliders.Contains(collider)) {
+                activeColliders.Add(collider);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "BoxHitCollider") {
+            ColliderScript collider = other.gameObject.GetComponent<ColliderScript>();
+            if (activeColliders.Contains(collider)) {
+                activeColliders.Remove(collider);
+            }
         }
     }
 }
